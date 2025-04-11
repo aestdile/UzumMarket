0b829103b235897abf9cf9f848bd96e684093e5f
aestdile@gmail.com <aestdile@gmail.com>
2025-04-11 20:37:19 +0500

    finished models

--- UzumMarket/Models/Order.cs
+++ UzumMarket/Models/Order.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UzumMarket.Models
 {
-    class Order
+    public class Order
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public string Status { get; set; } // Pending, Shipped, Delivered, Cancelled
+        public string PaymentStatus { get; set; } // Unpaid, Paid
+        public string ShippingAddress { get; set; }
+
     }
 }
