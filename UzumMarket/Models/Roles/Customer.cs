be3c29a43f7c1e8edb0163125c7cd9e4a93f1f46
aestdile@gmail.com <aestdile@gmail.com>
2025-04-11 20:36:01 +0500

    create roles models

--- UzumMarket/Models/Roles/Customer.cs
+++ UzumMarket/Models/Roles/Customer.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UzumMarket.Models.Roles
 {
-    class Customer
+    public class Customer
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public decimal Deposit { get; set; }
+        public decimal Balance { get; set; }
     }
 }
